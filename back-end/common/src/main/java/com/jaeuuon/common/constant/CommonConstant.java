eefb89992c7b0225d533411de735568ff774b7d2
jaeuuon <jaeuuon0157@outlook.com>
2025-09-13 19:13:56 +0900

    [back-end / common] 1. 공통 모듈 추가중.

--- back-end/common/src/main/java/com/jaeuuon/common/constant/CommonConstant.java
+++ back-end/common/src/main/java/com/jaeuuon/common/constant/CommonConstant.java
@@ -1,4 +1,13 @@
 package com.jaeuuon.common.constant;
 
+/**
+ * 공통 상수
+ */
 public class CommonConstant {
+
+    /**
+     * 프로젝트 최상위 그룹
+     */
+    public static final String GROUP = "com.jaeuuon";
+
 }
