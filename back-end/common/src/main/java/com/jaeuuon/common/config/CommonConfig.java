908c458f379531bcddfaf7be25f118730336dc80
jaeuuon <jaeuuon0157@outlook.com>
2025-09-14 13:39:02 +0900

    [back-end / common] 1. 공통 설정에 PropertiesScan, ObjectMapper 추가.

--- back-end/common/src/main/java/com/jaeuuon/common/config/CommonConfig.java
+++ back-end/common/src/main/java/com/jaeuuon/common/config/CommonConfig.java
@@ -1,7 +1,29 @@
 package com.jaeuuon.common.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jaeuuon.common.constant.CommonConstant;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
- * 공통 설정
+ * 공통 설정<br />
+ * ※ 프로젝트 최상위 그룹을 기반으로 설정 값을 바인딩
  */
+@ConfigurationPropertiesScan({CommonConstant.GROUP})
+@Configuration
 public class CommonConfig {
+
+    /**
+     * 직렬화 대상을 전역 변수로 한정
+     */
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE)
+                .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    }
+
 }
