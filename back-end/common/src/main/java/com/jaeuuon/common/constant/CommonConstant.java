7254e2df59c7d0387bec9489cadef9a82659690b
jaeuuon <jaeuuon0157@outlook.com>
2025-09-15 23:47:58 +0900

    [back-end / common] 1. 공통 설정, 상수, 헤더 주석 내용 보강.

--- back-end/common/src/main/java/com/jaeuuon/common/constant/CommonConstant.java
+++ back-end/common/src/main/java/com/jaeuuon/common/constant/CommonConstant.java
@@ -6,12 +6,12 @@ package com.jaeuuon.common.constant;
 public class CommonConstant {
 
     /**
-     * 클래스 스캔, 예외 추적 시 사용하는 최상위 패키지
+     * 클래스 스캔, 예외 추적 시 사용되는 최상위 패키지
      */
     public static final String BASE_PACKAGE = "com.jaeuuon";
 
     /**
-     * 문자열의 결합, 분리 시 사용하는 구분자
+     * 문자열의 결합, 분리 시 사용되는 구분자
      */
     public static final String DELIMITER = ",";
 
