a2765700efa60c3440b50977c97755ef82cdb1a5
jaeuuon <jaeuuon0157@outlook.com>
2025-09-15 10:53:22 +0900

    [back-end / common] 1. 공통 설정, 상수에 주석 작성 완료.

--- back-end/common/src/main/java/com/jaeuuon/common/config/CommonConfig.java
+++ back-end/common/src/main/java/com/jaeuuon/common/config/CommonConfig.java
@@ -10,14 +10,15 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 공통 설정<br />
- * ※
+ * ※ 최상위 패키지를 기반으로 @ConfigurationProperties 클래스를 스캔하여 빈으로 등록
  */
 @ConfigurationPropertiesScan({CommonConstant.BASE_PACKAGE})
 @Configuration
 public class CommonConfig {
 
     /**
-     * ObjectMapper의 직렬화 대상을 전역 변수로 한정
+     * ObjectMapper의 직렬화 대상을 전역 변수로 한정<br />
+     * ※ 응답 시 getter 등의 함수 포함 방지
      */
     @Bean
     public ObjectMapper objectMapper() {
--- back-end/common/src/main/java/com/jaeuuon/common/constant/CommonConstant.java
+++ back-end/common/src/main/java/com/jaeuuon/common/constant/CommonConstant.java
@@ -7,8 +7,14 @@ public class CommonConstant {
 
     /**
      * 최상위 패키지<br />
-     * ※
+     * ※ 클래스 스캔, 예외 추적 등에 사용
      */
     public static final String BASE_PACKAGE = "com.jaeuuon";
+
+    /**
+     * 문자열 구분자<br />
+     * ※ String 결합 또는 String[] 분리 시 사용
+     */
+    public static final String DELIMITER = ",";
 
 }
