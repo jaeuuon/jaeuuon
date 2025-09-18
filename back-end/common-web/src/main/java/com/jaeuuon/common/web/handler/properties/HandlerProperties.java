2859fcb8be068f7a5ce64f4526c35c6317dca5be
jaeuuon <jaeuuon0157@outlook.com>
2025-09-18 16:32:49 +0900

    [back-end / common-web] 1. 프로젝트 구조 정리중.

--- back-end/common-web/src/main/java/com/jaeuuon/common/web/handler/properties/HandlerProperties.java
+++ back-end/common-web/src/main/java/com/jaeuuon/common/web/handler/properties/HandlerProperties.java
@@ -1,4 +1,4 @@
 package com.jaeuuon.common.web.handler.properties;
 
-public class ResponseErrorProperties {
+public class HandlerProperties {
 }
---
+++ back-end/common/src/main/java/com/jaeuuon/common/exception/CommonException.java
@@ -0,0 +1,4 @@
+package com.jaeuuon.common.exception;
+
+public class CommonException {
+}
