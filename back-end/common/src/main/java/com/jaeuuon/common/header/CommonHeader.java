d8b8e01bddf98b2f4f5890b09c279e2f7b22aac3
jaeuuon <jaeuuon0157@outlook.com>
2025-09-23 09:28:12 +0900

    [back-end / common] 1. CommonHeader 주석 수정.

--- back-end/common/src/main/java/com/jaeuuon/common/header/CommonHeader.java
+++ back-end/common/src/main/java/com/jaeuuon/common/header/CommonHeader.java
@@ -7,7 +7,7 @@ public class CommonHeader {
 
     /**
      * 로깅 시 사용되는 요청 ID<br />
-     * ※ 외부의 요청이 gateway 프로젝트로 들어올 때 추가
+     * ※ gateway 프로젝트에서 외부의 요청이 들어올 때 추가
      *
      * <pre>
      *     {@code
@@ -27,7 +27,7 @@ public class CommonHeader {
 
     /**
      * 사용자 확인, 기록, 로깅 시 사용되는 사용자 ID<br />
-     * ※ 외부의 요청이 gateway 프로젝트로 들어올 때 JWT Access 또는 Refresh를 파싱하여 추가
+     * ※ gateway 프로젝트에서 외부의 요청이 들어올 때 JWT Access 또는 Refresh를 파싱하여 추가
      *
      * <pre>
      *     {@code
