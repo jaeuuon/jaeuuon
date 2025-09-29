21b84348e85212587e6bd1fff9193a4aa4cb9d72
jaeuuon <jaeuuon0157@outlook.com>
2025-09-29 14:17:44 +0900

    [back-end / common] 1. 공통 코드 인터페이스 추가.

--- back-end/common/src/main/java/com/jaeuuon/common/code/CommonCode.java
+++ back-end/common/src/main/java/com/jaeuuon/common/code/CommonCode.java
@@ -1,4 +1,59 @@
 package com.jaeuuon.common.code;
 
+/**
+ * 공통 코드<br />
+ * ※ 데이터베이스의 컬럼으로 사용되는 열거형 필드일 때 구현
+ */
 public interface CommonCode {
+
+    /**
+     * 열거형 필드와 매핑될 컬럼의 최대 길이
+     *
+     * <pre>
+     *     {@code
+     *     public class ExampleEntity {
+     *
+     *         // example_code VARCHAR(10)
+     *         @Column(length = CommonCode.LENGTH)
+     *         private ExampleCode exampleCode;
+     *
+     *     }
+     *     }
+     * </pre>
+     */
+    int LENGTH = 10;
+
+    /**
+     * 열거형 상수의 컬럼 값을 반환
+     *
+     * <pre>
+     *     {@code
+     *     public enum ExampleCode implements CommonCode {
+     *
+     *         A("a"), ...;
+     *
+     *         private final String value;
+     *
+     *         ExampleCode(String value) {
+     *             this.value = value;
+     *         }
+     *
+     *         public String getValue() {
+     *             return this.value;
+     *         }
+     *
+     *     }
+     *     }
+     * </pre>
+     * <pre>
+     *     {@code
+     *     // "a"
+     *     ExampleCode.A.getValue();
+     *     }
+     * </pre>
+     *
+     * @return {@code 컬럼 값}
+     */
+    String getValue();
+
 }
