b00de494fa96aea3d91100c42e09e89b08c84805
jaeuuon <jaeuuon0157@outlook.com>
2025-09-29 17:35:24 +0900

    [back-end / common] 1. 사용자 권한 코드 추가.

--- back-end/common/src/main/java/com/jaeuuon/common/code/user/UserAuthorityCode.java
+++ back-end/common/src/main/java/com/jaeuuon/common/code/user/UserAuthorityCode.java
@@ -1,4 +1,26 @@
 package com.jaeuuon.common.code.user;
 
-public enum UserAuthorityCode {
+import com.jaeuuon.common.code.CommonCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 사용자 권한 코드
+ */
+@Getter
+@RequiredArgsConstructor
+public enum UserAuthorityCode implements CommonCode {
+
+    /**
+     * 관리자
+     */
+    ADMIN("ADMIN"),
+
+    /**
+     * 일반 사용자
+     */
+    USER("USER");
+
+    private final String value;
+
 }
