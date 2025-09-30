a9e33c7bd0f4cb218bc86679883ef176da3cac3a
jaeuuon <jaeuuon0157@outlook.com>
2025-09-30 16:35:11 +0900

    [back-end / common] 1. 사용자 권한 코드 변환 함수 추가.

--- back-end/common/src/main/java/com/jaeuuon/common/code/user/UserAuthorityCode.java
+++ back-end/common/src/main/java/com/jaeuuon/common/code/user/UserAuthorityCode.java
@@ -1,8 +1,14 @@
 package com.jaeuuon.common.code.user;
 
 import com.jaeuuon.common.code.CommonCode;
+import com.jaeuuon.common.constant.CommonConstant;
+import com.jaeuuon.common.util.CommonUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 사용자 권한 코드
@@ -22,5 +28,33 @@ public enum UserAuthorityCode implements CommonCode {
     USER("USER");
 
     private final String value;
+
+    /**
+     * 결합된 이름 문자열을 분리 및 상수로 변환하여 반환<br />
+     * ※ 상수와 일치하지 않는 이름은 제외
+     *
+     * <pre>
+     *     {@code
+     *     // [UserAuthorityCode.ADMIN, UserAuthorityCode.USER]
+     *     UserAuthorityCode.convertToCodes("ADMIN,USER,UNKNOWN");
+     *     }
+     * </pre>
+     *
+     * @param names 결합된 이름 문자열
+     * @return {@code Set<UserAuthorityCode>}
+     */
+    public static Set<UserAuthorityCode> convertToCodes(@NonNull String names) {
+        Set<UserAuthorityCode> codes = new HashSet<>();
+
+        for (String name : names.split(CommonConstant.DELIMITER)) {
+            UserAuthorityCode code = CommonUtil.findEnum(UserAuthorityCode.class, name);
+
+            if (code != null) {
+                codes.add(code);
+            }
+        }
+
+        return codes;
+    }
 
 }
