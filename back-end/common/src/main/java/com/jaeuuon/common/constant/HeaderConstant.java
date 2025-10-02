be13c4dd8fb700ce5001f1d960f1ebcac2f288ec
jaeuuon <jaeuuon0157@outlook.com>
2025-10-02 13:26:16 +0900

    [back-end / common] 1. 권한 확인용 헤더명 추가.

--- back-end/common/src/main/java/com/jaeuuon/common/constant/HeaderConstant.java
+++ back-end/common/src/main/java/com/jaeuuon/common/constant/HeaderConstant.java
@@ -1,5 +1,7 @@
 package com.jaeuuon.common.constant;
 
+import com.jaeuuon.common.code.user.UserAuthorityCode;
+
 /**
  * 요청, 응답 헤더
  */
@@ -44,5 +46,28 @@ public class HeaderConstant {
      * </pre>
      */
     public static final String USER_ID = "User-ID";
+
+    /**
+     * 사용자의 권한 확인 시 사용되는 사용자 권한 코드 상수들의 결합된 이름<br />
+     * ※ gateway 프로젝트에서 외부의 요청이 들어올 때 JWT Access를 파싱하여 추가
+     *
+     * <pre>
+     *     {@code
+     *     // "ADMIN,USER"
+     *     serverHttpRequest.getHeaders()
+     *             .getFirst(HeaderConstant.USER_AUTHORITIES);
+     *     }
+     * </pre>
+     * <pre>
+     *     {@code
+     *     // "ADMIN,USER"
+     *     httpServletRequest.getHeader(HeaderConstant.USER_AUTHORITIES);
+     *     }
+     * </pre>
+     *
+     * @see UserAuthorityCode
+     * @see UserAuthorityCode#convertToCodes(String)
+     */
+    public static final String USER_AUTHORITIES = "User-Authorities";
 
 }
